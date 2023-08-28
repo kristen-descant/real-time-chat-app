d259fc2af4f4c36906b63e2eb8f883daeaaff4b8
noah tate <noah.tater@gmail.com>
2023-08-28 14:42:42 -0600

    backend put together

---
+++ backend/comments_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CommentsAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'comments_app'
---
+++ backend/forum_topics_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ForumTopicsAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'forum_topics_app'
