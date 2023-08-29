aa2ae7cb912ac7627c517e03ad63559f5ea3af01
kristen-descant <kristendescant01@gmail.com>
2023-08-29 17:42:59 -0400

    all links besides user added to navbar

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
 
   return (
     <>
+      <Navbar/>
       <Outlet />
     </>
   );
