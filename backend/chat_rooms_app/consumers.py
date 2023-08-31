49efe799654e46106222869a02f5bcb8610de8c8
della955 <dellaowens9@gmail.com>
2023-08-31 14:52:31 -0400

    added user message db update

--- backend/chat_rooms_app/consumers.py
+++ backend/chat_rooms_app/consumers.py
@@ -5,11 +5,22 @@ from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 from .models import ChatRoom
 from users_app.models import User
+from messages_app.models import Message
 
 class ChatConsumer(WebsocketConsumer):
+    user_id = ""
+    chat_room_id = "" 
+
+    def updated_message_db(self):
+        messages = ChatRoom.objects.get(self.chat_room_id).sort()[:50] 
+        self.send(text_data=json.dumps({"messages":messages}))
+
     def connect(self):
-        # user_id = self.scope.get("headers", {}).get(b'users', b"").decode("utf-8")
         
+        self.users_id = self.scope.get("headers", {}).get(b'users', b"").decode("utf-8")
+        user1 = self.users_id.filter('user1')
+        user2 = self.users_id.filter('user2')
+
         try:
             # user = User.objects.get(id=user_id)
             self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
@@ -17,14 +28,20 @@ class ChatConsumer(WebsocketConsumer):
 
             # Fetch or create a ChatRoom instance
             chat_room, created = ChatRoom.objects.get_or_create()
+            self.chat_room_id = chat_room.id 
             # chat_room.users.add(user)  # Add the user to the users field
 
             # Join room group
             async_to_sync(self.channel_layer.group_add)(
                 self.room_group_name, self.channel_name
             )
-
-            self.accept()
+           
+            if created == True:
+                self.accept()
+            else:
+                self.updated_message_db()
+                self.accept()
+          
         except User.DoesNotExist:
             # Handle user not found error
             pass
@@ -39,8 +56,10 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         # user = User.objects.get(id=user_id)
         # user.messages.add(message)  # Add the user to the users field
-
+        user = User.objects.get(id=self.user_id).messages.add(message)
+        Message.create(content=message, chat_room=self.chat_room_id)
         text_data_json = json.loads(text_data)
+        
         message = text_data_json["message"]
 
         # Send message to room group
@@ -56,3 +75,4 @@ class ChatConsumer(WebsocketConsumer):
 
         # Send message to WebSocket
         self.send(text_data=json.dumps({"message": message}))
+
