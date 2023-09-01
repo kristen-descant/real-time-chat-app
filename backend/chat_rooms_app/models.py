06c2c28a877977e3c5a5eec97b35f2aa4639ed33
noah tate <noah.tater@gmail.com>
2023-08-31 19:51:11 -0600

    updated chat_room models and consumers

--- backend/chat_rooms_app/models.py
+++ backend/chat_rooms_app/models.py
@@ -6,7 +6,7 @@ class ChatRoom(models.Model):
     id = models.BigAutoField(primary_key=True)
     room_id = models.CharField(max_length=255, unique=True)
     created = models.DateTimeField(auto_now_add=True)
-    users = models.ForeignKey(User, related_name="chat_room")
+    users = models.ForeignKey(User,on_delete=models.CASCADE, related_name="chat_room")
     # You can remove the "messages" field
 
     def __str__(self):
