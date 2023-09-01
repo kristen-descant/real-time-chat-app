66a113b70c50dfe11e3e98881b68b073b3f077df
kristen-descant <kristendescant01@gmail.com>
2023-09-01 11:19:57 -0400

    made some changes for testing purposes

--- backend/chat_rooms_app/consumers.py
+++ backend/chat_rooms_app/consumers.py
@@ -63,7 +63,7 @@ class ChatConsumer(WebsocketConsumer):
             if created:
                 self.accept()
             else:
-                self.updated_message_db()
+                # self.updated_message_db()
                 self.accept()
         
         except User.DoesNotExist:
@@ -81,18 +81,17 @@ class ChatConsumer(WebsocketConsumer):
         # user = User.objects.get(id=user_id)
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-        user = User.objects.get(text_data_json["user"])
+        # user = User.objects.get(text_data_json["user"])
         ###########TEXT_DATA.user expects a json key value pair of "user":"user.id"###############
-        user.messages.add(message)
+        # user.messages.add(message)
         ##########################################################################################
-        Message.create(content=message, chat_room=self.chat_room_id, sender=user)        
+        # Message.create(content=message, chat_room=self.chat_room_id, sender=user)        
 
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             ############################################## "message":f"{username}:{message}" to make it more of a chat room. 
             # This edits the message itself, so in the chat_message function below it will be message = message, displaying as message : {username}:{message}.
-            self.room_group_name, {"type": "chat.message", "message": f"{self.user.display_name}: {message}"} 
-        )
+            self.room_group_name, {"type": "chat.message", "message": message}        )
 
     # Receive message from room group
     def chat_message(self, event):
