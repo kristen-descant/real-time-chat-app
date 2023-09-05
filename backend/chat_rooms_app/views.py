a827dfe657887f88c6b946a2fb81c40e86c17a5f
noah tate <noah.tater@gmail.com>
2023-09-05 15:40:36 -0600

    fixed get view for chatroom.

--- backend/chat_rooms_app/views.py
+++ backend/chat_rooms_app/views.py
@@ -18,11 +18,11 @@ def room(request, room_name):
 @api_view(['GET'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
-def updated_message_db(self, oldest_message_id=0):
+def updated_message_db(room_name, oldest_message_id=0):
     if oldest_message_id==0:
-        oldest_message_id = ChatRoom.objects.get(id=self.chat_room_id).messages.all().order_by('-id').first()     #this sets it to the most recent message.       
+        oldest_message_id = ChatRoom.objects.get(id=room_name).messages.all().order_by('-id').first()     #this sets it to the most recent message.       
     
-    retrieved_messages = ChatRoom.objects.get(id=self.chat_room_id).messages.filter(date__lt=Message.objects.get(oldest_message_id).date).order_by('-date')[:50]
+    retrieved_messages = ChatRoom.objects.get(id=room_name).messages.filter(date__lt=Message.objects.get(oldest_message_id).date).order_by('-date')[:50]
     serializer = MessageSerializer(retrieved_messages)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
