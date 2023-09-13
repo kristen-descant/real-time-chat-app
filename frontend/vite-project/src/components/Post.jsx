ebb99a05164198d0c4dba1971ab47b5279710b1d
EthanBullington <bullington.c.ethan@gmail.com>
2023-09-13 14:31:45 -0400

    Styling for the comments is complete.

--- frontend/vite-project/src/components/Post.jsx
+++ frontend/vite-project/src/components/Post.jsx
@@ -3,66 +3,64 @@ import { useEffect, useState } from "react";
 import { api } from "../pages/utility";
 import thumbsUp from "../media/pngwing.com (2).png";
 import thumbsDown from "../media/pngwing.com (4).png";
-import trash from "../media/trash.png"
+import trash from "../media/trash.png";
 
 export default function Post({ post }) {
   const { forum_id, post_id } = useParams();
   const navigate = useNavigate();
-  const [currentPost, setCurrentPost] = useState('');
-  const [editing, setEditing] = useState(false)
-  const [editedPost, setEditedPost] = useState('')
-  const [editedTitle, setEditedTitle] = useState('')
+  const [currentPost, setCurrentPost] = useState("");
+  const [editing, setEditing] = useState(false);
+  const [editedPost, setEditedPost] = useState("");
+  const [editedTitle, setEditedTitle] = useState("");
   const { userInfo, counter, setCounter } = useOutletContext();
   // console.log(post)
 
   useEffect(() => {
-    const getPost = async() => {
+    const getPost = async () => {
       let token = localStorage.getItem("token");
-      api.defaults.headers.common["Authorization"] = `Token ${token}`
-      let response = await api.get(`posts/${forum_id}/posts/${post_id}/`)
+      api.defaults.headers.common["Authorization"] = `Token ${token}`;
+      let response = await api.get(`posts/${forum_id}/posts/${post_id}/`);
       // console.log(response.data)
-      setCurrentPost(response.data)
-    }
-    if(post_id) {
-      getPost()
-
+      setCurrentPost(response.data);
+    };
+    if (post_id) {
+      getPost();
     }
   }, []);
 
   const editPost = async (e) => {
+    e.stopPropagation();
     if (post_id) {
       let response = await api.put(`posts/${forum_id}/posts/${post_id}/`, {
         content: editedPost,
         title: editedTitle,
-        edited: true
+        edited: true,
       });
     } else {
       let response = await api.put(`posts/${forum_id}/posts/${post.id}/`, {
         content: editedPost,
         title: editedTitle,
-        edited: true
+        edited: true,
       });
     }
-    setCounter(counter+1)
-  }
+    setCounter(counter + 1);
+  };
 
   const deletePost = async () => {
     try {
       if (post_id) {
-      let response = await api.delete(`posts/${forum_id}/posts/${post_id}/`);
-      navigate(`/forum/${forum_id}`)
+        let response = await api.delete(`posts/${forum_id}/posts/${post_id}/`);
+        navigate(`/forum/${forum_id}`);
       } else {
         let response = await api.delete(`posts/${forum_id}/posts/${post.id}/`);
       }
-      setCounter(counter+1)
+      setCounter(counter + 1);
+    } catch {
+      alert("Post could not be deleted");
     }
-    catch {
-      alert("Post could not be deleted")
-    }
-  }
-  
-  
-// console.log(currentPost.title)
+  };
+
+  // console.log(currentPost.title)
 
   // console.log(currentPost)
   const upVote = async (e) => {
@@ -75,12 +73,11 @@ export default function Post({ post }) {
         up: userInfo.data.id,
       });
     }
-    setCounter(counter+1)
+    setCounter(counter + 1);
 
     // navigate(`/`)
-    
   };
-  
+
   const downVote = async (e) => {
     if (post_id) {
       let response = await api.put(`posts/${forum_id}/post/${post_id}/`, {
@@ -91,7 +88,7 @@ export default function Post({ post }) {
         down: userInfo.data.id,
       });
     }
-    setCounter(counter+1)
+    setCounter(counter + 1);
   };
   // console.log(post.edited)
   return (
@@ -119,33 +116,60 @@ export default function Post({ post }) {
             </div>
             <div className="flex flex-row">
               <span className="mr-2 text-[red]">{post.down.length}</span>
-              <img className="h-3 md:h-5" src={thumbsDown} alt="thumbs down"
-              onClick={(e) => downVote(e)} />
+              <img
+                className="h-3 md:h-5"
+                src={thumbsDown}
+                alt="thumbs down"
+                onClick={(e) => downVote(e)}
+              />
             </div>
           </div>
-          <img className='absolute top-1 right-1 h-6 w-6' src={trash} onClick={()=> deletePost()}/>
-          <div className="absolute bottom-1 left-1">Edited : {post.edited.toString()}</div>
-          {userInfo && (<button className="border-2 rounded p-1 absolute bottom-1 right-1 disabled:border-0 disabled:text-color_palette_3 hover:bg-color_palette_1" disabled={post.created_by != userInfo.data.id} onClick={() => {setEditing(true)}}>Edit Post</button>)}
+          <img
+            className="absolute top-1 right-1 h-6 w-6"
+            src={trash}
+            onClick={() => deletePost()}
+          />
+          <div className="absolute bottom-1 left-1">
+            Edited : {post.edited.toString()}
+          </div>
+          {userInfo && (
+            <button
+              className={`p-0.5 rounded text-xs absolute bottom-1 right-1 ${
+                post.created_by !== userInfo.data.id ? 'border-0' : 'border'
+              } disabled:text-color_palette_3 hover:bg-color_palette_1 ${
+                post.created_by !== userInfo.data.id ? 'opacity-0 pointer-events-none' : ''
+              }`}
+              disabled={post.created_by != userInfo.data.id}
+              onClick={() => {
+                setEditing(true);
+              }}
+            >
+              Edit Post
+            </button>
+          )}
           {editing && (
             <form
-            className="flex flex-col overflow-auto"
-            onSubmit={(e) => editPost(e)}
-          >
-            <input
-              className="mb-2 text-center border-[black] border-2 rounded"
-              type="text"
-              placeholder="title"
-              value={editedTitle}
-              onChange={(e) => setEditedTitle(e.target.value)}
-            />
-            <textarea
-              className="mb-2 flex flex-wrap text-center border-[black] border-2 rounded"
-              placeholder="content (limit 255 chracters)"
-              value={editedPost}
-              onChange={(e) => setEditedPost(e.target.value)}
-            />
-            <input className="border border-b-color_palette_5 bg-color_palette_3 rounded pl-2 pr-1 hover:bg-color_palette_1 " type="submit" />
-          </form>
+              className="flex flex-col overflow-auto"
+              onSubmit={(e) => editPost(e)}
+            >
+              <input
+                className="mb-2 text-center border-[black] border-2 rounded"
+                type="text"
+                placeholder="title"
+                value={editedTitle}
+                onChange={(e) => setEditedTitle(e.target.value)}
+              />
+              <textarea
+                className="mb-2 flex flex-wrap text-center border-[black] border-2 rounded"
+                placeholder="content (limit 255 chracters)"
+                value={editedPost}
+                onChange={(e) => setEditedPost(e.target.value)}
+              />
+              <input
+                className="border border-b-color_palette_5 bg-color_palette_3 rounded pl-2 pr-1 hover:bg-color_palette_1 "
+                type="submit"
+              />
+            </form>
           )}
         </div>
       ) : (
@@ -159,61 +183,97 @@ export default function Post({ post }) {
             {currentPost.title}
           </div>
           <div className="flex flex-row justify-around w-1/3 mt-2">
-          {!currentPost ? (
-            <div className="flex flex-row">
-              <span className="mr-2 text-[green]">{currentPost.up}</span>
-                <img
-                  className="h-3 md:h-5"
-                  src={thumbsUp}
-                  alt="thumbs up"
-                  onClick={(e) => upVote(e)}
-                />
-            </div>):(
-            <div className="flex flex-row">
-              <span className="mr-2 text-[green]">{currentPost.up.length}</span>
-                <img
-                  className="h-3 md:h-5"
-                  src={thumbsUp}
-                  alt="thumbs up"
-                  onClick={(e) => upVote(e)}
-                />
-            </div>)}
-          
             {!currentPost ? (
-            <div className="flex flex-row">
-              <span className="mr-2 text-[red]">{currentPost.down}</span>
-              <img className="h-3 md:h-5" src={thumbsDown} alt="thumbs down" 
-              onClick={(e) => downVote(e)}/>
-            </div>
-            ):(<div className="flex flex-row">
-            <span className="mr-2 text-[red]">{currentPost.down.length}</span>
-            <img className="h-3 md:h-5" src={thumbsDown} alt="thumbs down" 
-            onClick={(e) => downVote(e)}/>
-          </div>)
-            }
+              <div className="flex flex-row">
+                <span className="mr-2 text-[green]">{currentPost.up}</span>
+                <img
+                  className="h-3 md:h-5"
+                  src={thumbsUp}
+                  alt="thumbs up"
+                  onClick={(e) => upVote(e)}
+                />
+              </div>
+            ) : (
+              <div className="flex flex-row">
+                <span className="mr-2 text-[green]">
+                  {currentPost.up.length}
+                </span>
+                <img
+                  className="h-3 md:h-5"
+                  src={thumbsUp}
+                  alt="thumbs up"
+                  onClick={(e) => upVote(e)}
+                />
+              </div>
+            )}
+
+            {!currentPost ? (
+              <div className="flex flex-row">
+                <span className="mr-2 text-[red]">{currentPost.down}</span>
+                <img
+                  className="h-3 md:h-5"
+                  src={thumbsDown}
+                  alt="thumbs down"
+                  onClick={(e) => downVote(e)}
+                />
+              </div>
+            ) : (
+              <div className="flex flex-row">
+                <span className="mr-2 text-[red]">
+                  {currentPost.down.length}
+                </span>
+                <img
+                  className="h-3 md:h-5"
+                  src={thumbsDown}
+                  alt="thumbs down"
+                  onClick={(e) => downVote(e)}
+                />
+              </div>
+            )}
           </div>
-          <img className='absolute top-1 right-1 h-6 w-6' src={trash} onClick={()=> deletePost()}/>
-          {userInfo && (<button className="border-2 rounded p-1 absolute bottom-1 right-1 disabled:border-0 disabled:text-color_palette_3" disabled={currentPost.created_by != userInfo.data.id} onClick={() => {setEditing(true)}}>Edit Post</button>)}
+          <img
+            className="absolute top-1 right-1 h-6 w-6"
+            src={trash}
+            onClick={() => deletePost()}
+          />
+          {userInfo && (
+            <button
+              className={`p-0.5 rounded text-xs absolute bottom-1 right-1 ${
+                currentPost.created_by !== userInfo.data.id ? 'border-0' : 'border'
+              } disabled:text-color_palette_3 hover:bg-color_palette_1 ${
+                currentPost.created_by !== userInfo.data.id ? 'opacity-0 pointer-events-none' : ''
+              }`}
+              disabled={currentPost.created_by != userInfo.data.id}
+              onClick={() => {
+                setEditing(true);
+              }}
+            >
+              Edit Post
+            </button>
+          )}
           {editing && (
             <form
-            className="flex flex-col overflow-auto"
-            onSubmit={(e) => editPost(e)}
-          >
-            <input
-              className="mb-2 text-center border-[black] border-2 rounded"
-              type="text"
-              placeholder="title"
-              value={editedTitle}
-              onChange={(e) => setEditedTitle(e.target.value)}
-            />
-            <textarea
-              className="mb-2 flex flex-wrap text-center border-[black] border-2 rounded"
-              placeholder="content (limit 255 chracters)"
-              value={editedPost}
-              onChange={(e) => setEditedPost(e.target.value)}
-            />
-            <input className="border border-b-color_palette_5 bg-color_palette_3 rounded pl-2 pr-1 hover:bg-color_palette_1 " type="submit" />
-          </form>
+              className="flex flex-col overflow-auto"
+              onSubmit={(e) => editPost(e)}
+            >
+              <input
+                className="mb-2 text-center border-[black] border-2 rounded"
+                type="text"
+                placeholder="title"
+                value={editedTitle}
+                onChange={(e) => setEditedTitle(e.target.value)}
+              />
+              <textarea
+                className="mb-2 flex flex-wrap text-center border-[black] border-2 rounded"
+                placeholder="content (limit 255 chracters)"
+                value={editedPost}
+                onChange={(e) => setEditedPost(e.target.value)}
+              />
+              <input
+                className="border border-b-color_palette_5 bg-color_palette_3 rounded pl-2 pr-1 hover:bg-color_palette_1 "
+                type="submit"
+              />
+            </form>
           )}
         </div>
       )}
